03d5d6701244bcd4a59c8219e31b66dad733d96f
nickdandave <nickdandavidson@gmail.com>
2021-08-13 05:09:49 -0400

    Rework WP files for Jekyll

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,171 +3,119 @@ document.addEventListener('DOMContentLoaded', function() {
     (function($, window) {
         "use strict";
 
-        const body = $("body"),
-              nav = $(".nav"),
-              navLinks = $(".nav__list--item"),
-              navToggle = $(".nav__toggle");
-
-        const siteMainScript = {
-            handleNav: {
+        const atainsh = {
+            navToggle: {
                 test() {
                     return true;
                 },
                 run() {
+                    // global vars
+                    const nav = $(".nav-main__list"),
+                          navLinks = $(".nav-main__list--item"),
+                          body = $("body"),
+                          contentWrapper = $(".content-wrapper"),
+                          contentRow = $(".content-wrapper>.wrapper"),
+                          mainCarousel = $("#mainCarousel"),
+                          workVimeoContainer = $(".hero-slide__work--head-vimeo"),
+                          workVimeo = $(".hero-slide__work--head-vimeo iframe"),
+                          modalBG = $(".modal-background"),
+                          modalClose = $(".modal-close__button"),
+                          photosCarousel = $("#photos-grid"),
+                          photosModalContainer = $(".hero-slide__photos--modal"),
+                          photosModalClose = $(".hero-slide__photos--modal-close"),
+                          modalOfMe = $("#modalCarouselOfMe"),
+                          photosOfMe = $(".photos-OF-me").find(".hero-slide__photos--grid-photo"),
+                          modalByMe = $("#modalCarouselByMe"),
+                          photosByMe = $(".photos-BY-me").find(".hero-slide__photos--grid-photo");
+
+                    function setVimeo() {
+                        if ($(workVimeo)[0].hasAttribute("src") && $(workVimeo).attr("src").includes("vimeo")) {
+                            const workVimeoPlayer = new Vimeo.Player(workVimeo);
+
+                            workVimeoPlayer.on("playing", function openVimeoModal() {
+                                body.addClass("modal-open");
+                                // contentWrapper.addClass("modal-open");
+                                workVimeoContainer.addClass("modal-open");
+                                // modalBG.addClass("modal-open");
+                              });
+
+                            modalClose.on("click", function closeVimeoModal() {
+                                workVimeoPlayer.pause();
+                                body.removeClass("modal-open");
+                                // contentWrapper.removeClass("modal-open");
+                                workVimeoContainer.removeClass("modal-open");
+                                // modalBG.removeClass("modal-open");
+                            });
+                        }
+                    }
+
                     function setupNav() {
-                        navToggle.on("click", function toggleNav() {
-                            navToggle.toggleClass("nav-open");
-                            nav.toggleClass("nav-open");
+                        $(navLinks[0]).addClass("nav-pane--active");
+
+                        navLinks.on("click", function toggleContentView() {
+                            let thisSection = $(this).data("nav-to"),
+                                thisSlide = $(mainCarousel.children(".carousel-inner").children(".carousel-item")[thisSection]),
+                                activeSlide = mainCarousel.children(".carousel-inner").children(".carousel-item.active");
+
+                            $(".nav-pane--active").removeClass("nav-pane--active");
+                            $(this).addClass("nav-pane--active");
+
+                            mainCarousel.animate({opacity: 0}, 200).delay(400).animate({opacity: 1}, 200);
+                            setTimeout(function() {
+                                activeSlide.removeClass("active");
+                                thisSlide.addClass("active");
+                                $(document).scrollTop(0);
+                            }, 400);
                         });
                     }
 
+                    function handlePhotos() {
+                        photosCarousel.on("slide.bs.carousel", function fadeCarousel(event) {
+                            let currentGrid = $(event.relatedTarget),
+                                nextGrid = $(currentGrid.siblings()[0]);
+
+                            currentGrid.removeClass("in-transition");
+                            nextGrid.addClass("in-transition");
+                        });
+                    }
+
+                    function handlePhotoModals() {
+                        photosOfMe.on("click", function() {
+                            let thisSlideToggle = $(this).data("slide-toggle");
+
+                            body.addClass("modal-open");
+                            photosModalContainer.addClass("modal-open");
+                            modalOfMe.carousel(parseInt(thisSlideToggle));
+                            modalOfMe.addClass("modal-open");
+                        });
+
+                        photosByMe.on("click", function() {
+                            let thisSlideToggle = $(this).data("slide-toggle");
+
+                            body.addClass("modal-open");
+                            photosModalContainer.addClass("modal-open");
+                            modalByMe.carousel(parseInt(thisSlideToggle));
+                            modalByMe.addClass("modal-open");
+                        });
+
+                        photosModalClose.on("click", function() {
+                            body.removeClass("modal-open");
+                            photosModalContainer.removeClass("modal-open");
+                            modalOfMe.hasClass("modal-open") ? modalOfMe.removeClass("modal-open") : modalByMe.removeClass("modal-open");
+                        });
+                    }
+
+                    setVimeo();
                     setupNav();
-                }
-            },
-            pantonePage: {
-                test() {
-                    return $(".pantone__stack").length;
-                },
-                run() {
-                    const colorStack = $(".pantone__stack"),
-                          stackPosition = colorStack.offset().top,
-                          pantoneColors = $(".pantone__color"),
-                          firstColor = pantoneColors.first().find("h1").text(),
-                          lastColor = pantoneColors.last().find("h1").text(),
-                          nextButton = $(".pantone__buttons--button.next-color"),
-                          backButton = $(".pantone__buttons--button.prev-color"),
-                          topButton = $(".pantone__buttons--button.back-to-top"),
-                          bottomButton = $(".pantone__buttons--button.go-to-bottom");
-
-                    let _scrollTimeout = null,
-                        userIsWheeling = false;
-
-                    function swipeOnStack() {
-                        colorStack.addClass("fixed-position");
-                        body.addClass("overflow-hidden");
-                        $(window).scrollTop(stackPosition);
-                        nextButton.addClass("active");
-                        bottomButton.addClass("active");
-
-                        window.addEventListener("wheel", watchWheel);
-                        $(window).on("keydown", watchArrows);
-                    }
-
-                    function swipeOffStack() {
-                        colorStack.removeClass("fixed-position");
-                        body.removeClass("overflow-hidden");
-                        window.removeEventListener("wheel", watchWheel);
-                        $(window).off("keydown");
-                        $(window).scrollTop(stackPosition - 50);
-                        $(".pantone__buttons--button").removeClass("active");
-                    }
-
-                    function swipeNext() {
-                        $(".current-color").removeClass("current-color").addClass("color-before");
-                        $(".color-before").last().next().addClass("current-color");
-
-                        let currentIndex = $(".current-color").index();
-
-                        if (currentIndex > 0 && !backButton.hasClass("active")) {
-                            backButton.addClass("active");
-                            topButton.addClass("active");
-                        }
-                        if (currentIndex == $(".pantoneColors").last().index() - 1) {
-                            nextButton.removeClass("active");
-                            bottomButton.removeClass("active");
-                        }
-                    }
-
-                    function swipePrev() {
-                        $(".current-color").prev().removeClass("color-before").addClass("current-color");
-                        $(".current-color").first().next().removeClass("current-color");
-
-                        let currentIndex = $(".current-color").index();
-
-                        if (currentIndex == 0 && backButton.hasClass("active")) {
-                            backButton.removeClass("active");
-                            topButton.removeClass("active");
-                        }
-                        if (currentIndex < $(".pantoneColors").last().index() && !nextButton.hasClass("active")) {
-                            nextButton.addClass("active");
-                            bottomButton.addClass("active");
-                        }
-                    }
-
-                    function watchWheel(e) {
-                        let delta = ((typeof e.wheelDelta != "undefined") ? (-e.deltaY) : e.detail);
-
-                        if (!userIsWheeling) {
-                            userIsWheeling = true;
-                            let currentColor = $(".current-color").find("h1").text();
-
-                            if (delta > 0 && currentColor == firstColor) {
-                                swipeOffStack();
-                            } else if (delta < 0 && currentColor !== lastColor) {
-                                swipeNext();
-                            } else if (delta > 0) {
-                                swipePrev();
-                            }
-                        }
-
-                        clearTimeout(_scrollTimeout);
-                        _scrollTimeout = setTimeout(function setWheelingStatus() {
-                            userIsWheeling = false;
-                        }, 66);
-                    };
-
-                    function watchArrows(e) {
-                        let currentColor = $(".current-color").find("h1").text();
-
-                        if (e.which === 38 && currentColor == firstColor) {
-                            swipeOffStack();
-                        } else if (e.which === 40 && currentColor !== lastColor) {
-                            swipeNext();
-                        } else if (e.which === 38) {
-                            swipePrev();
-                        }
-                    }
-
-                    function handleColorStack() {
-                        $(window).on("scroll", function watchScroll() {
-                            let scrollPos = window.scrollY;
-
-                            if (scrollPos >= stackPosition && !colorStack.hasClass("fixed-position")) {
-                                swipeOnStack();
-                            }
-                        });
-
-                        nextButton.on("click", swipeNext);
-                        backButton.on("click", swipePrev);
-                        topButton.on("click", function() {
-                            let currColorCount = $(".current-color").index(),
-                                i;
-                            for (i = currColorCount; i >= 0; i--) {
-                                swipePrev();
-                            }
-                            swipeOffStack();
-                        });
-                        bottomButton.on("click", function() {
-                            let numColors = pantoneColors.last().index(),
-                                thisColor = $(".current-color").index(),
-                                j;
-
-                            for (j = 0; j < numColors - thisColor; j++) {
-                                swipeNext();
-                            }
-                            nextButton.removeClass("active");
-                            bottomButton.removeClass("active");
-                        });
-                    }
-
-                    handleColorStack();
+                    handlePhotos();
+                    handlePhotoModals();
                 }
             }
         };
 
-        for (let key in siteMainScript) {
-            if (siteMainScript[key].test()) {
-                siteMainScript[key].run();
+        for (let key in atainsh) {
+            if (atainsh[key].test()) {
+                atainsh[key].run();
             }
         }
 
